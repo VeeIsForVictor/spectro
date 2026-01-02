0577d530e2acdeae73b49cbbed2e7bc455058a57
Victor Edwin Reyes <victorereyesgg@gmail.com>
2026-01-03 03:56:40 +0800

    fix: correct paths to $lib

--- src/lib/server/models/discord/thread-channel.ts
+++ src/lib/server/models/discord/thread-channel.ts
@@ -1,7 +1,7 @@
 import { type InferOutput, number, object, optional, string } from 'valibot';
-import { Channel } from './channel';
-import { Snowflake } from './snowflake';
-import { ThreadMetadata } from './thread-metadata';
+import { Channel } from '$lib/server/models/discord/channel';
+import { Snowflake } from '$lib/server/models/discord/snowflake';
+import { ThreadMetadata } from '$lib/server/models/discord/thread-metadata';
 
 export const ThreadChannel = object({
   ...Channel.entries,
